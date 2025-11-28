e15cf770e69ea9ab2cdb18f436d2fc394846b1b7
Justin Foley <justin.foley@joinmavely.com>
2025-11-28 11:03:18 -0500

    chore: add commitlint and husky for commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			['feat', 'fix', 'docs', 'chore', 'refactor', 'test', 'style', 'perf', 'ci']
+		]
+	}
+};
+
